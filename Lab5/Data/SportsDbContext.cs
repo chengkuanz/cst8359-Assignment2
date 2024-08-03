839866964a581354ae4f8e7f973c33da45a7080a
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-08-03 14:51:46 -0400

    add lab5

---
+++ Lab5/Data/SportsDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using EntityFramework.Models;
+//using System.Collections.Generic;
+using Lab5.Models;
+//need this other wise got error The type or namespace name 'Subscription' could not be found (are you missing a using directive or an assembly reference?)
+
+namespace Lab5.Data
+{
+    public class SportsDbContext : DbContext
+    {
+        public SportsDbContext(DbContextOptions<SportsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fan> Fans { get; set; }
+        public DbSet<SportClub> SportClubs { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Call base class implementation
+            base.OnModelCreating(modelBuilder);
+
+            // Define table names
+            modelBuilder.Entity<Fan>().ToTable("Fan");
+            modelBuilder.Entity<SportClub>().ToTable("SportClub");
+            modelBuilder.Entity<Subscription>().ToTable("Subscription");
+
+           
+        }
+    }
+
+  
+}
---
+++ Lab5/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab5.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public int FanId { get; set; }
+        public string SportClubId { get; set; }
+
+        // Navigational properties
+        public Fan Fan { get; set; }
+        public SportClub SportClub { get; set; }
+
+
+    }
+}
