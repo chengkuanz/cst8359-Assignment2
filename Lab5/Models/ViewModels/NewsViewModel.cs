ca387095aec459979dc5e1667171d4d4fcf3a6ef
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-08-05 16:54:54 -0400

    Create NewsViewModel.cs

---
+++ Lab5/Models/ViewModels/NewsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Lab5.Models.ViewModels
+{
+    public class NewsViewModel
+    {
+       public IEnumerable<News> News { get; set; }
+        
+        public SportClub SportClub { get; set; }
+    }
+}
